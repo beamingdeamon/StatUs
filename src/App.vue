e5dba38adb2b88e319c0ff2ac6d1c54925d0906e
RerrorHandler <61464119+RerrorHandler@users.noreply.github.com>
2020-10-27 19:30:14 +0600

    header
    add

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Header />
+    <SubHeader />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import SubHeader from './components/SubHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    SubHeader
+  }
+}
+</script>
+
+<style>
+ul {
+  list-style-type: disc;
+  margin-block-start: 0px;
+  margin-block-end: 0px;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  padding-inline-start: 0px;
+}
+li {
+  list-style-type: none;
+}
+a {
+  text-decoration: none
+}
+body {
+  margin: 0px;
+}
+</style>
---
+++ src/components/SubHeader.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="subheader">
+        <ul class="subheader-wrapper">
+            <li class="subheader-menu">
+                <a class="subheader-menu__link" id="education" href="#">оброзование</a>
+                <ul class="link-item" id="item-education">
+                    <li>item__education</li>
+                    <li>item__education</li>
+                    <li>item__education</li>
+                    <li>item__education</li>
+                </ul>
+            </li>
+            <li class="subheader-menu">
+                <a class="subheader-menu__link" href="#">оброзование</a>
+                <ul class="link-item">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>   
+            </li>
+            <li class="subheader-menu">
+                <a class="subheader-menu__link" href="#">оброзование</a>
+                <ul class="link-item">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul> 
+            </li>
+            <li class="subheader-menu">
+                <a class="subheader-menu__link" href="#">оброзование</a>
+                <ul class="link-item">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul> 
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style>
+.subheader {
+    width: 100%;
+    height: 50px;
+    background-size: cover;
+    background-color: #0273b2;
+    position: sticky;
+    display: flex;
+    justify-content: center;
+}
+.subheader-wrapper {
+    margin: 0px;
+}
+.subheader-menu {
+    list-style-type: none;
+    display: inline-block;
+    height: 4.5vh;
+    line-height: 4.5vh;
+}
+.subheader-menu__link {
+    height: 100%;
+    padding: 0 2.0vh;
+    color: #fff;
+    background-color: #0273b2;
+    text-decoration: none;
+    font-size: 1.5vh;
+    text-transform: uppercase;
+    position: relative;
+    letter-spacing: 0.1vh;
+}
+.link-item {
+    display: none;
+}
+
+</style>
